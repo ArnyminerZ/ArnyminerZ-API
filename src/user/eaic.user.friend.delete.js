7f3d07dff4769042243118fbc709b4ab5106a455
Arnau Mora Gras <arnyminer.z@gmail.com>
2020-10-19 15:30:10 +0200

    :sparkles: Implemented async `findUser`.

--- src/user/eaic.user.friend.delete.js
+++ src/user/eaic.user.friend.delete.js
@@ -1,4 +1,6 @@
 const {sendNotification} = require("../utils/FirebaseUtils");
+const {querySync} = require('../utils/mysql-sync')
+const {findUser} = require('../utils/UserUtils')
 
 module.exports = class FriendDelete {
     constructor(messaging, mysql) {
@@ -6,9 +8,40 @@ module.exports = class FriendDelete {
         this.messaging = messaging
     }
 
-    process(request, response) {
+    async process(request, response) {
         const params = request.params;
-        const messaging = this.messaging
+        const messaging = this.messaging;
+        const mysql = this.mysql;
+
+        const checkFriendsUserSql = "SELECT `id` FROM `EscalarAlcoiaIComtat`.`friends` WHERE NOT `deleted`='1' AND (`user_id`='{0}' AND `friend_id`='{1}') OR (`user_id`='{1}' AND `friend_id`='{0}') LIMIT 1;"
+        const checkFriendsFirebaseSql = "SELECT `id` FROM `EscalarAlcoiaIComtat`.`friends` WHERE NOT `deleted`='1' AND (`user_uid`='{0}' AND `friend_uid`='{1}') OR (`user_uid`='{1}' AND `friend_uid`='{0}') LIMIT 1;"
+
+        const deleteSql = "UPDATE `EscalarAlcoiaIComtat`.`friends` SET `deleted`='1' WHERE `id`='{0}' LIMIT 1;";
+
+        try {
+            // First, check if both users exist
+            // First check if caller exists
+            const caller = await findUser(mysql, params.user)
+            if (caller == null)
+                return response.status(400).send({error: 'user-doesnt-exist'});
+
+            // Then, check if called exists
+            const called = await findUser(mysql, params.other)
+            if (called == null)
+                return response.status(400).send({error: 'friend-doesnt-exist'});
+
+            // Now check if users are friends
+            let friendsCheck = await querySync(mysql, checkFriendsUserSql.format(caller.id, called.id))
+            if (friendsCheck.length <= 0)
+                friendsCheck = await querySync(mysql, checkFriendsFirebaseSql.format(caller.firebase_uid, called.firebase_uid))
+            if (friendsCheck.length <= 0)
+                response.status(400).send({error: 'users-are-not-friends'});
+
+            await querySync(mysql, deleteSql.format(friendsCheck[0].id))
+            response.status(200).send({result: "ok"})
+        } catch (e) {
+            response.status(500).send({error: e});
+        }
 
         const sql = "UPDATE `EscalarAlcoiaIComtat`.`friends` SET `deleted`='1' WHERE `user_uid`='{0}' AND `friend_uid`='{1}' OR `user_uid`='{1}' AND `friend_uid`='{0}' LIMIT 1;"
             .format(params.user, params.other);
--- src/user/eaic.user.friend.request.js
+++ src/user/eaic.user.friend.request.js
@@ -1,6 +1,8 @@
 const {v4: uuidv4} = require('uuid');
 
 const {sendNotification} = require("../utils/FirebaseUtils");
+const {querySync} = require('../utils/mysql-sync')
+const {findUser} = require('../utils/UserUtils')
 
 module.exports = class FriendRequest {
     constructor(messaging, mysql) {
@@ -8,29 +10,53 @@ module.exports = class FriendRequest {
         this.mysql = mysql
     }
 
-    process(request, response) {
+    async process(request, response) {
         const params = request.params;
         const messaging = this.messaging;
+        const mysql = this.mysql;
 
-        const ip = request.connection.remoteAddress;
-        const agent = request.get('User-Agent');
-        const uuid = uuidv4();
+        if (params.user === params.other)
+            return response.status(400).send({error: 'cannot-ask-same'});
 
-        /*const checkAlreadySql = "SELECT `id` FROM `EscalarAlcoiaIComtat`.`friend_requests` (uuid, from_user, to_user, remote_address, agent) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}');"
-            .format(uuid, params.user);*/
-        const sql = "INSERT INTO `EscalarAlcoiaIComtat`.`friend_requests` (uuid, from_user, to_user, remote_address, agent) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}');"
-            .format(uuid, params.user, params.other, ip, agent);
+        const checkFriendsSql = "SELECT `id` FROM `EscalarAlcoiaIComtat`.`friends` WHERE NOT `deleted`='1' AND `user_id`='{0}' AND `friend_id`='{1}';"
+        const requestSql = "INSERT INTO `EscalarAlcoiaIComtat`.`friend_requests` (uuid, from_user_id, to_user_id, remote_address, agent) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}');"
+        try {
+            // First check if caller exists
+            const caller = await findUser(mysql, params.user)
+            if (caller == null)
+                return response.status(400).send({error: 'user-doesnt-exist'});
 
-        this.mysql.query(sql, function (error, result) {
-            if (error)
-                response.status(500).send({error: error});
-            else {
-                sendNotification(messaging, params.other, "*new_friend_request", "", {
-                    from_uid: params.user
-                })
+            // Then, check if called exists
+            const called = await findUser(mysql, params.other)
+            if (called == null)
+                return response.status(400).send({error: 'friend-doesnt-exist'});
 
-                response.status(200).send({result: "ok", uuid: uuid})
-            }
-        })
+            // Now let's check if users are already friends
+            const alreadyFriends = await querySync(mysql, checkFriendsSql.format(caller.id, called.id))
+            if (alreadyFriends.length > 0)
+                return response.status(400).send({error: 'users-already-friends'});
+
+            // Get some parameters to fill
+            const ip = request.connection.remoteAddress;
+            const agent = request.get('User-Agent');
+            const uuid = uuidv4();
+            // Execute the call
+            await querySync(mysql, requestSql.format(uuid, caller.id, called.id, ip, agent))
+
+            // Send notification through the old firebase-uid-based receiver system
+            sendNotification(messaging, called.firebase_uid, "*new_friend_request", "", {
+                from_uid: caller.firebase_uid,
+                from_user: caller.id
+            })
+            // Send notification through the new user-[id] based receiver system
+            sendNotification(messaging, 'user-' + called.id, "*new_friend_request", "", {
+                from_uid: caller.firebase_uid,
+                from_user: called.id
+            })
+
+            response.status(200).send({result: "ok", uuid: uuid})
+        } catch (e) {
+            response.status(500).send({error: e});
+        }
     }
 }
